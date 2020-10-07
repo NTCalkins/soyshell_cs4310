20f036c8f822062ed3719597df883b1cdad54e9a
hlin91 <hlin91@outlook.com>
2020-10-07 12:12:57 -0700

    Split off parsing function of evalExpr into another function. New function needs testng and evalExpr needs to be heavily rewritten

--- src/Parser.c
+++ src/Parser.c
@@ -100,28 +100,40 @@ char* evalArg(char *s)
     /* TODO: Implement this */
 }
 
-/* Evaluate the command */
+/* Evaluate the command or run the executable */
 int evalCmd(char *cmd, char *args, char *inBuff, char *outBuff)
 {
     /* TODO: Implement this */
     bool background = false; /* Run this command as a background process */
+    if (args[strlen(args) - 1] == '&')
+    {
+        background = true;
+        args[strlen(args - 1)] = '\0';
+    }
+    if (isCmd(cmd)) /* Received a command */
+    {
+        /* TODO: Run the appropriate command */
+    }
+    /* TODO: Run the executable */
     
 }
 
 /* Evaluate the statement */
 int evalS(char *s, int pos1, int pos2, char *inBuff, char *outBuff)
 {
+    /* TODO: Needs to be rewritten to support proper piping */
     /* Substrings to store command and args */
     char cmd[BUFF_MAX] = "";
     char args[BUFF_MAX] = "";
     int tempPos = pos1;
+    /* Trim whitespace */
     while (pos2 > pos1 && isspace(s[pos2]))
         --pos2;
     while (pos1 <= pos2 && isspace(s[pos1]))
         ++pos1;
     if (pos1 > pos2)
         return 0;
-    /* Assume that all whitespace has been trimmed and braces have been properly matched */
+    /* Assume braces have been properly matched */
     if (s[pos1] == '{') /* Braced expression */
     {
         /* Remove the braces */
@@ -141,20 +153,21 @@ int evalS(char *s, int pos1, int pos2, char *inBuff, char *outBuff)
     return evalCmd(cmd, args, inBuff, outBuff);
 }
 
-/* Evaluate the expression. Assume that there are no trailing whitespaces */
-int evalExpr(char *expr, int pos1, int pos2, char *inBuff, char *outBuff)
+/* Parse the given expression into a left statement, operator, and right expression */
+int parseExpr(char *expr, char *s, char *op, char *e)
 {
+    /* TODO: Needs testing */
+    int pos1 = 0;
+    int pos2 = strlen(expr) - 1;
     int i = INVALID_POS;
     int sEnd = INVALID_POS; /* End position of statement */
     int opPos1 = INVALID_POS; /* Start and end positions for operator */
     int opPos2 = INVALID_POS;
     int expStart = INVALID_POS;
-    int r = FATAL_ERR; /* Return value */
-    char op[BUFF_MAX] = ""; /* The operator */
     char token[BUFF_MAX]; /* The space delineated token we are considering */
     int tokPos1, tokPos2;
-    if ((pos2 - pos1 + 1) == 0) /* Empty expression passed */
-        return 0;
+    if (strlen(expr) == 0) /* Empty expression passed */
+        return false;
     while (pos2 > pos1 && isspace(expr[pos2])) /* Remove trailing whitespace */
         --pos2;
     while (pos1 <= pos2 && isspace(expr[pos1])) /* Ignore leading whitespace */
@@ -200,102 +213,113 @@ int evalExpr(char *expr, int pos1, int pos2, char *inBuff, char *outBuff)
         fprintf(stderr, "parse error: expected argument after operator \'%s\'\n", op);
         return FATAL_ERR;
     }
-    strncpy(op, expr + opPos1, opPos2 - opPos1 + 1);
+    strncpy(op, expr + opPos1, opPos2 - opPos1 + 1); /* Store the operator */
     sEnd = opPos1 - 1;
+    strncpy(s, expr + pos1, sEnd - pos1 + 1); /* Store the statement */
     while (i <= pos2 && isspace(expr[i])) /* Move past white space seperation */
         ++i;
     expStart = i; /* Mark the start of the right hand expression */
-    /* Resolve the operator */
-    if (strcmp(op, "&&") == 0) /* Logical AND */
-    {
-        r = evalS(expr, pos1, sEnd, inBuff, outBuff);
-        if (r == FATAL_ERR) /* Propogate fatal errors */
-            return FATAL_ERR;
-        if (r == 0) /* Left hand statement succeeded */
-            return evalExpr(expr, expStart, pos2, inBuff, outBuff); /* Evaluate right hand expression */
-        else /* Return false if first statement fails */
-            return 1;
-    }
-    else if (strcmp(op, "||") == 0) /* Logical OR */
-    {
-        r = evalS(expr, pos1, sEnd, inBuff, outBuff);
-        if (r == FATAL_ERR)
-            return FATAL_ERR;
-        if (r == 1) /* Left hand statement failed */
-            return evalExpr(expr, expStart, pos2, inBuff, outBuff);
-        else /* Return true if first statement succeeds */
-            return 0;
-    }
-    else if (strcmp(op, "|") == 0) /* Pipe */
-    {
-        char pipeBuff[BUFF_MAX] = ""; /* Intermediate buffer to do the piping */
-        r = evalS(expr, pos1, sEnd, inBuff, pipeBuff);
-        if (r == FATAL_ERR)
-            return FATAL_ERR;
-        return evalExpr(expr, expStart, pos2, pipeBuff, outBuff);
-    }
-    else if (strcmp(op, ";") == 0) /* Evaluate sequentually. Idk if this should be treated as an operator tbh */
-    {
-        r = evalS(expr, pos1, sEnd, inBuff, outBuff);
-        if (r == FATAL_ERR)
-            return FATAL_ERR;
-        return evalExpr(expr, expStart, pos2, inBuff, outBuff);
-    }
-    else if (strcmp(op, "<") == 0) /* Input redirection */
-    {
-        /* Right hand expression is a file name */
-        char fileName[BUFF_MAX] = {};
-        strncpy(fileName, expr + expStart, pos2 - expStart + 1);
-        FILE *inFile = fopen(fileName, "r");
-        if (inFile == NULL) /* Could not open file */
-        {
-            fprintf(stderr, "Could not open file \'%s\'\n", fileName);
-            return FATAL_ERR;
-        }
-        /* Get the file size */
-        fseek(inFile, 0, SEEK_END);
-        long fSize = ftell(inFile);
-        rewind(inFile);
-        char *newIn = (char*)malloc(sizeof(char) * fSize); /* New input buffer that contains the contents of the file */
-        long readSize = fread(newIn, 1, fSize, inFile); /* Read in contents of file into buffer */
-        if (readSize != fSize)
-        {
-            fprintf(stderr, "Could not read contents of file \'%s\'\n", fileName);
-            return FATAL_ERR;
-        }
-        fclose(inFile);
-        r = evalS(expr, pos1, sEnd, newIn, outBuff);
-        free(newIn);
-        return r;
-    }
-    else if (strcmp(op, "<<") == 0) /* Here document */
-    {
-        char delim[BUFF_MAX] = {}; /* Delimiter string signaling end of input */
-        char buffer[BUFF_MAX] = {};
-        char input[BUFF_MAX] = {};
-        strncpy(delim, expr + expStart, pos2 - expStart + 1);
-        fgets(input, BUFF_MAX, stdin);
-        while (strcmp(input, delim) != 0) /* While we have not encountered the delim string */
-            strcat(buffer, input); /* Cat input into buffer */
-        return evalS(expr, pos1, sEnd, buffer, outBuff);
-    }
-    else if (strcmp(op, ">") == 0 || strcmp(op, ">>") == 0) /* Output redirection */
-    {
-        char fileName[BUFF_MAX] = "";
-        strncpy(fileName, expr + expStart, pos2 - expStart + 1);
-        char newOut[BUFF_MAX] = "";
-        r = evalS(expr, pos1, sEnd, inBuff, newOut);
-        FILE *outFile = (strlen(op) == 1) ? fopen(fileName, "w") : fopen(fileName, "a");
-        if (outFile == NULL)
-        {
-            fprintf(stderr, "Could not open file \'%s\'", fileName);
-            return FATAL_ERR;
-        }
-        fwrite(newOut, sizeof(char), sizeof(newOut), outFile);
-        fclose(outFile);
-        return r;
-    }
-    /* Execution should never reach here */
-    fprintf(stderr, "evalExpr: tried to evaluate invalid operator\n");
-    return FATAL_ERR;
+    strncpy(e, expr + expStart, pos2 - expStart + 1);
+}
+
+/* TODO: Rewrite this to pipe properly */
+/* Evaluate the expression. Assume that there are no trailing whitespaces */
+int evalExpr(char *expr, int pos1, int pos2, char *inBuff, char *outBuff)
+{
+    /* TODO: Rewrite this to support proper piping */
+    /* if (strcmp(op, "&&") == 0) /\* Logical AND *\/ */
+    /* { */
+    /*     r = evalS(expr, pos1, sEnd, inBuff, outBuff); */
+    /*     if (r == FATAL_ERR) /\* Propogate fatal errors *\/ */
+    /*         return FATAL_ERR; */
+    /*     if (r == 0) /\* Left hand statement succeeded *\/ */
+    /*         return evalExpr(expr, expStart, pos2, inBuff, outBuff); /\* Evaluate right hand expression *\/ */
+    /*     else /\* Return false if first statement fails *\/ */
+    /*         return 1; */
+    /* } */
+    /* else if (strcmp(op, "||") == 0) /\* Logical OR *\/ */
+    /* { */
+    /*     r = evalS(expr, pos1, sEnd, inBuff, outBuff); */
+    /*     if (r == FATAL_ERR) */
+    /*         return FATAL_ERR; */
+    /*     if (r == 1) /\* Left hand statement failed *\/ */
+    /*         return evalExpr(expr, expStart, pos2, inBuff, outBuff); */
+    /*     else /\* Return true if first statement succeeds *\/ */
+    /*         return 0; */
+    /* } */
+    /* else if (strcmp(op, "|") == 0) /\* Pipe *\/ */
+    /* { */
+    /*     char pipeBuff[BUFF_MAX] = ""; /\* Intermediate buffer to do the piping *\/ */
+    /*     r = evalS(expr, pos1, sEnd, inBuff, pipeBuff); */
+    /*     if (r == FATAL_ERR) */
+    /*         return FATAL_ERR; */
+    /*     return evalExpr(expr, expStart, pos2, pipeBuff, outBuff); */
+    /* } */
+    /* else if (strcmp(op, ";") == 0) /\* Evaluate sequentually. Idk if this should be treated as an operator tbh *\/ */
+    /* { */
+    /*     r = evalS(expr, pos1, sEnd, inBuff, outBuff); */
+    /*     if (r == FATAL_ERR) */
+    /*         return FATAL_ERR; */
+    /*     return evalExpr(expr, expStart, pos2, inBuff, outBuff); */
+    /* } */
+    /* else if (strcmp(op, "<") == 0) /\* Input redirection *\/ */
+    /* { */
+    /*     /\* Right hand expression is a file name *\/ */
+    /*     char fileName[BUFF_MAX] = {}; */
+    /*     strncpy(fileName, expr + expStart, pos2 - expStart + 1); */
+    /*     FILE *inFile = fopen(fileName, "r"); */
+    /*     if (inFile == NULL) /\* Could not open file *\/ */
+    /*     { */
+    /*         fprintf(stderr, "Could not open file \'%s\'\n", fileName); */
+    /*         return FATAL_ERR; */
+    /*     } */
+    /*     /\* Get the file size *\/ */
+    /*     fseek(inFile, 0, SEEK_END); */
+    /*     long fSize = ftell(inFile); */
+    /*     rewind(inFile); */
+    /*     char *newIn = (char*)malloc(sizeof(char) * fSize); /\* New input buffer that contains the contents of the file *\/ */
+    /*     long readSize = fread(newIn, 1, fSize, inFile); /\* Read in contents of file into buffer *\/ */
+    /*     if (readSize != fSize) */
+    /*     { */
+    /*         fprintf(stderr, "Could not read contents of file \'%s\'\n", fileName); */
+    /*         return FATAL_ERR; */
+    /*     } */
+    /*     fclose(inFile); */
+    /*     r = evalS(expr, pos1, sEnd, newIn, outBuff); */
+    /*     free(newIn); */
+    /*     return r; */
+    /* } */
+    /* else if (strcmp(op, "<<") == 0) /\* Here document *\/ */
+    /* { */
+    /*     char delim[BUFF_MAX] = {}; /\* Delimiter string signaling end of input *\/ */
+    /*     char buffer[BUFF_MAX] = {}; */
+    /*     char input[BUFF_MAX] = {}; */
+    /*     strncpy(delim, expr + expStart, pos2 - expStart + 1); */
+    /*     fgets(input, BUFF_MAX, stdin); */
+    /*     while (strcmp(input, delim) != 0) /\* While we have not encountered the delim string *\/ */
+    /*     { */
+    /*         strcat(buffer, input); /\* Cat input into buffer *\/ */
+    /*         fgets(input, BUFF_MAX, stdin); */
+    /*     } */
+    /*     return evalS(expr, pos1, sEnd, buffer, outBuff); */
+    /* } */
+    /* else if (strcmp(op, ">") == 0 || strcmp(op, ">>") == 0) /\* Output redirection *\/ */
+    /* { */
+    /*     char fileName[BUFF_MAX] = ""; */
+    /*     strncpy(fileName, expr + expStart, pos2 - expStart + 1); */
+    /*     char newOut[BUFF_MAX] = {}; */
+    /*     r = evalS(expr, pos1, sEnd, inBuff, newOut); */
+    /*     FILE *outFile = (strlen(op) == 1) ? fopen(fileName, "w") : fopen(fileName, "a"); */
+    /*     if (outFile == NULL) */
+    /*     { */
+    /*         fprintf(stderr, "Could not open file \'%s\'", fileName); */
+    /*         return FATAL_ERR; */
+    /*     } */
+    /*     fwrite(newOut, sizeof(char), sizeof(newOut), outFile); */
+    /*     fclose(outFile); */
+    /*     return r; */
+    /* } */
+    /* /\* Execution should never reach here *\/ */
+    /* fprintf(stderr, "evalExpr: tried to evaluate invalid operator\n"); */
+    /* return FATAL_ERR; */
 }
