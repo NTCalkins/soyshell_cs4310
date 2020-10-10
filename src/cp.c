cceb381be903a54b9db86ea7c78fdbaad2fabef3
Nick Calkins <36932560+NTCalkins@users.noreply.github.com>
2020-10-10 16:14:15 -0700

    Finished cp.c
    Standalone like the others, next order of business is tying these all together with a main runner

---
+++ src/cp.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char** argv) {
+	
+	if (argc < 3) {
+		printf("Not enough arguments\n");
+		return 0;
+	}
+
+	FILE *fp_source = fopen(argv[1], "r");
+
+	if (!fp_source) {
+		printf("No file %s exists\n",argv[1]);
+		return 0;
+	}
+	
+	//We're copying over this file anyways, clean opening
+	remove(argv[2]);
+	FILE *fp_dest = fopen(argv[2], "w");
+
+	char c;
+
+	while (( c = fgetc(fp_source)) != EOF) {
+		fputc(c,fp_dest);
+	}
+	fclose(fp_source);
+	fclose(fp_dest);
+	
+	return 1;
+}
