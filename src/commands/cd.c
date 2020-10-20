427689c20a15a058cae4c424e5bda7f979df9c64
hlin91 <hlin91@outlook.com>
2020-10-19 20:57:43 -0700

    Made command mute on success

--- src/commands/cd.c
+++ src/commands/cd.c
@@ -16,16 +16,12 @@ int main(int argc, char **argv)
     }
 
     char cwd[1024];
-    
-    printf("Old working directory: %s\n", getcwd(cwd, sizeof(cwd)));
 
     // Changes directory still? I think?
     if (chdir(argv[1]) == -1) {
         perror("Failed to change directory.");
         return -1;
     }
-
-    printf("Current working directory: %s\n", getcwd(cwd, sizeof(cwd)));
 
     return 0;
 }
