75e45a69e315265683e3877ac8fab0e8faf1c3f4
Nick <nicholastcalkins@gmail.com>
2020-10-20 22:17:42 -0700

    updated rm to remove files as well

--- src/commands/rm.c
+++ src/commands/rm.c
@@ -1,5 +1,6 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main (int argc, char **argv)
 {
@@ -9,8 +10,11 @@ int main (int argc, char **argv)
     }
     int r = rmdir(argv[1]);
     if (r == -1) {
-        printf("%s could not be removed: either not empty or nonexistent\n", argv[1]);
-        return 1;
+        int r = remove(argv[1]);
+        if (r == -1) {
+            printf("%s could not be removed: either not empty or nonexistent\n", argv[1]);
+            return 1;
+        }
     }
     return 0;
 }
