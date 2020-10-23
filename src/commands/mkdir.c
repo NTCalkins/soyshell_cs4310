1ac982418c506b44fa813b97674536541c2921af
Nick <nicholastcalkins@gmail.com>
2020-10-23 12:11:21 -0700

    functionality for rmdir and mkdir to create multiple directories at once

--- src/commands/mkdir.c
+++ src/commands/mkdir.c
@@ -5,18 +5,19 @@
 int main(int argc, char **argv)
 {
     int status;
-    if (argc != 2)
+    if (argc < 2)
     {
 	    puts("No arguments given");
 	    return 1;
     }
-    status = mkdir(argv[1],0700);
-    if (status == -1)
-    {
-        if (errno == EEXIST) {
-            puts("Error: directory already exists");
+    for (int i = 1; i < argc; i++) {
+        status = mkdir(argv[i],0700);
+        if (status == -1)
+        {
+            if (errno == EEXIST) {
+                printf("Error: directory %s already exists\n",argv[i]);
+            }
         }
-        return 1;
     }
     return 0;
 }
--- src/commands/rmdir.c
+++ src/commands/rmdir.c
@@ -4,16 +4,18 @@
 
 int main (int argc, char **argv)
 {
-    if (argc != 2) {
-        puts("No directory given");
+    if (argc < 2) {
+        puts("No directory/directories given");
         return 1;
     }
-    int r = rmdir(argv[1]);
-    if (r == -1) {
-        int r = remove(argv[1]);
+    for (int i = 1; i < argc; i++) {
+        int r = rmdir(argv[i]);
         if (r == -1) {
-            printf("%s could not be removed: either not empty or nonexistent\n", argv[1]);
-            return 1;
+            int r = remove(argv[i]);
+            if (r == -1) {
+                printf("%s could not be removed: either not empty or nonexistent\n", argv[i]);
+                return 1;
+            }
         }
     }
     return 0;
