1e75871622b202e9c7ade042b4a8b8d224741104
Nick <nicholastcalkins@gmail.com>
2020-10-23 21:01:34 -0700

    changed pwd to not work with arguments

--- src/commands/pwd.c
+++ src/commands/pwd.c
@@ -3,9 +3,13 @@
 #include <stdlib.h>
 #include <limits.h>
 
-int main(int argv, char **argc)
+int main(int argc, char **argv)
 {
 	char *cwd = NULL;
+        if (argc != 1 ) {
+                puts("pwd: too many arguments");
+                return -1;
+        }
         cwd = getcwd(cwd, PATH_MAX);
         puts(cwd);
         free(cwd);
